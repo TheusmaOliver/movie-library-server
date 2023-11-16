2ee5db98292cc556a352fc22a4b17929f270095f
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-11-16 17:28:12 -0300

    feat: create Movie table

---
+++ src/main/java/com/theusma/movielibrary/entities/Movie.java
@@ -0,0 +1,31 @@
+package com.theusma.movielibrary.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_movies")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private String category;
+    private Double score;
+    private String year;
+    private String description;
+}
