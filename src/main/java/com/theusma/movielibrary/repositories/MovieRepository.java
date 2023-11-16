bca7fe8d84600c8413d81cd435ab71503f2cb29c
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-11-16 17:29:03 -0300

    feat: create Movie Repository

---
+++ src/main/java/com/theusma/movielibrary/repositories/MovieRepository.java
@@ -0,0 +1,11 @@
+package com.theusma.movielibrary.repositories;
+
+import com.theusma.movielibrary.entities.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, UUID> {
+}
