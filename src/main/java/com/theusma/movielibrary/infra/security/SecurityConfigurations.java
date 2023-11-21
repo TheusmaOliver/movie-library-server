1bbc1992d4d1f5724e911db69c56b00d53d00d2b
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-11-21 17:50:11 -0300

    chore: allowing everyone to access the movies route

--- src/main/java/com/theusma/movielibrary/infra/security/SecurityConfigurations.java
+++ src/main/java/com/theusma/movielibrary/infra/security/SecurityConfigurations.java
@@ -27,8 +27,8 @@ public class SecurityConfigurations {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register","/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/movies").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/admin/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/movies").hasRole("ADMIN")
                         .anyRequest().authenticated()
