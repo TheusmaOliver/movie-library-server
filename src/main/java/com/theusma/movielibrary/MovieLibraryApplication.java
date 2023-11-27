c9c120b80c542dc77b88802fbdbebb19069c41ac
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-11-27 18:31:17 -0300

    feat: email service created

--- src/main/java/com/theusma/movielibrary/MovieLibraryApplication.java
+++ src/main/java/com/theusma/movielibrary/MovieLibraryApplication.java
@@ -2,8 +2,10 @@ package com.theusma.movielibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MovieLibraryApplication {
 
 	public static void main(String[] args) {
