b136bbf99fcdd5e57a388e4fcd868a69af46b9af
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-12-01 19:31:17 -0300

    chore: adding repository bean in user repository

--- src/main/java/com/theusma/movielibrary/repositories/UserRepository.java
+++ src/main/java/com/theusma/movielibrary/repositories/UserRepository.java
@@ -3,7 +3,9 @@ package com.theusma.movielibrary.repositories;
 import com.theusma.movielibrary.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     UserDetails findByUsername(String username);
 }
