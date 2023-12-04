5919979286aad9ee42740e9f1e7eeb17ef71eae2
TheusmaOliver <matheusrodriguesoliveira273@gmail.com>
2023-12-04 13:10:33 -0300

    chore: change user repository response

--- src/main/java/com/theusma/movielibrary/repositories/UserRepository.java
+++ src/main/java/com/theusma/movielibrary/repositories/UserRepository.java
@@ -2,12 +2,11 @@ package com.theusma.movielibrary.repositories;
 
 import com.theusma.movielibrary.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
-    UserDetails findByUsername(String username);
+    User findByUsername(String username);
 }
